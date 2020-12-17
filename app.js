19a7fd58ab9df43871ab2d53dd9a4d5894c4ce05
severalpens <paulcollinsaf@gmail.com>
2020-12-17 21:23:14 +1000

    main

--- app.js
+++ app.js
@@ -4,41 +4,41 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express();
 const cors = require('cors');
+const app = express();
+app.use(cors());
 
 const indexRouter = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 
 app.use(function (req, res, next) {
-
+  
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
-
+  
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', '*');
-
+  
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', '*');
-
+  
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
+  
   // Pass to next layer of middleware
   next();
 });
 
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
