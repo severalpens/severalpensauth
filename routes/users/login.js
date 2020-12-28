a636dcf4d11149bab763da0f2a0e149282b4ffda
severalpens <paulcollinsaf@gmail.com>
2020-12-28 14:18:46 +1000

    main: Fix login

--- routes/users/login.js
+++ routes/users/login.js
@@ -9,39 +9,32 @@ var jwt = require("jsonwebtoken");
 router.use(cors());
 router.use(bodyParser.json());
 
-router.post("/", function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+async function addLog(username,result){
+  let log = new LogsModel();
+  log.type = 'login';
+  log.username = username;
+  log.result = result;
+  log.save();
+}
 
-  try {
-    let log = new LogsModel();
-    log.type = 'login';
-    let username = req.body.username;
-    log.username = username;
-    log.save();
-    let tmpPassword = req.body.password;
-    UsersModel.findOne({ username }, (err, user) => {
-      if (err) {
-        return res.send(err);
-      }
-      bcrypt.compare(tmpPassword, user.password, (err, success) => {
-        if (err) {
-          log.result = err;
-          return res.send(err);
-        }
-        else{
-          log.result = success;
-        }
-        log.save()
-        res.send({
-          unit: user.unit,
-          user_id: user._id,
-          username: user.username,
-          token: user.token,
-        });
-      });
+router.post("/", async function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  let username = req.body.username;
+  let user = await UsersModel.findOne({username}).exec();
+  if(user){
+    let result = await bcrypt.compare(req.body.password, user.password);
+    addLog(username, result);  
+    res.send({
+      unit: user.unit,
+      user_id: user._id,
+      username: user.username,
+      token: user.token,
     });
-  } catch (err) {
-    return res.send(err);
+  }
+  else{
+    addLog(username, result);  
+    res.statusCode = 401;
+    res.send('User not found');
   }
 });
 
